d5a74330c8a26f3844da7e15e61d07a70c9cc5a3
mobnew <mob_new@mail.ru>
2022-08-27 16:12:21 +0300

    move userHomer

--- Navigation/Profile/Auth/Checker.swift
+++ Navigation/Profile/Auth/Checker.swift
@@ -12,11 +12,12 @@ class Checker {
     
     private let loginPrivate: String = "rr"
     private let passPrivate: String = "qq"
+    private let userHomer = User(login: "homer", fullName: "Homer Simpson", status: "Sleep", avatar: UIImage(named: "h2")!)
     var user: User?
     
     func check(login: String, pass: String) -> Bool {
         if (login == loginPrivate) && (pass == passPrivate) {
-             user = User(login: "homer", fullName: "Homer Simpson", status: "Sleep", avatar: UIImage(named: "h2")!)
+             user = userHomer
             return true
         } else {
             return false
